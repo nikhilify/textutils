6dcd312bfa67b062102e160b352c9bc532cb30e1
nikhilify <144203855+nikhilify@users.noreply.github.com>
2024-01-09 21:48:20 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
-    <App />
-    </HashRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
