8bf5759cbd288f31d8dc112af3b4bd7a9b904937
nikhilify <144203855+nikhilify@users.noreply.github.com>
2024-01-09 21:20:37 +0530

    2nd commit
    udated routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-// import About from './components/About';
+import About from './components/About';
 import React,{useState} from 'react';
 // import TodoForm from './components/TodoForm';
-// import {
-//   BrowserRouter as Router,
-//   Route,
-//   Routes
-// } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode ] = useState('light');
@@ -28,21 +29,22 @@ function App() {
   }
   return (
 <>
-  <Navbar title="textutils"  mode={mode} toggleMode={toggleMode}/>
   
-  <div className='container my-3' >
-  <TextForm heading="Enter text to analyze" upper="Convert to Uppercase" mode={mode}/>
-  </div>
+  {/* <TextForm heading="Enter text to analyze" upper="Convert to Uppercase" mode={mode}/> */}
 
-    {/* <About mode={mode}></About> */}
-        {/* <Routes>
-          <Route exact path="/about" element={<About mode={mode}/>}>
-          </Route>
-          <Route exact path="/" element={<TextForm heading="Enter text to analyze" upper="Convert to Uppercase" mode={mode}/>}>
+      <BrowserRouter>
+      <Navbar title="textutils"  aboutText="About" mode={mode} toggleMode={toggleMode}/>
+ 
+    <div className='container my-3' >
 
-          </Route>
+        <Routes>
+          <Route exact path="/about" element={<About mode={mode}/>}></Route>
+          <Route exact path="/" element={<TextForm heading="Enter text to analyze" upper="Convert to Uppercase" mode={mode}/>}/>
         </Routes>
-    </Router> */}
+      </div>
+
+    </BrowserRouter>
+
 </>
   )
 };
